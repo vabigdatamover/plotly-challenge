2b7b3c50ee038c442af1a58d0aa160e5dcbeec11
vabigdatamover <vabigdatamover@gmail.com>
2019-09-25 16:16:35 -0400

    Plotly Challenge

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -3,10 +3,12 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
+  const metadataURL = "/metadata/" +sample;
+  d3.json(metadataURL).then((data) => {
+  // Use d3 to select the panel with id of `#sample-metadata`
+    metadatapanel = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-
+    metadatapanel.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
